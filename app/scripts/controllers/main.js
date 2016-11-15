b5ce8677ef895fc164c7617893d5ef42deb80d9c
Michael Crowley <miketcrowley@gmail.com>
2016-11-15 13:17:21 -0500

    got an LFO working

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,12 +10,25 @@
 angular.module('synthApp')
   .controller('MainCtrl', function ($scope, Synth) {
 
-  	$scope.types = ['sine', 'square', 'sawtooth', 'triangle'];
-    $scope.changeType = Synth.changeType;
+	$scope.type = 'sine';
+	$scope.types = ['sine', 'square', 'sawtooth', 'triangle'];
+  $scope.changeType = Synth.changeType;
+  $scope.changeLfoType = Synth.changeLfoType;
+
+  $scope.frequency = 0;
 	$scope.changeFrequency = Synth.changeFrequency;
+	$scope.changeFrequency(0);
+	$scope.step = function(frequency){return (frequency/1000);};
+	
 	$scope.volume = 0;
 	$scope.changeVolume = Synth.changeVolume;
 	$scope.changeVolume(0);
-    Synth.oscillator.start();
+  Synth.oscillator.start();
+
+  $scope.filterFrequency = Synth.filterFrequency;
+
+  $scope.lfoRate = 0;
+  $scope.changeRate = Synth.changeRate;
+  // $scope.changeRate(0);
 
   });
--- app/scripts/factories/synth-factory.js
+++ app/scripts/factories/synth-factory.js
@@ -8,17 +8,21 @@ angular.module('synthApp')
 		var context = new $window.AudioContext();
 		var _oscillator = context.createOscillator();
 		var _gainNode = context.createGain();
+		var _filter = context.createBiquadFilter();
+		_filter.type = _filter.LOWPASS;
+		var _masterFilter = context.createBiquadFilter();
+		_filter.frequency.value = 300;
+		_masterFilter.frequency.value = 5000;
 
-		_oscillator.connect(_gainNode);
-		_gainNode.connect(context.destination);
 		
 
 		_gainNode.gain.value = 0.01;
 		_oscillator.frequency.value = 60;
-		_oscillator.type = 'square';
+		_oscillator.type = 'sine';
 
 		SynthFactory.oscillator = _oscillator;
 		SynthFactory.gain = _gainNode;
+		SynthFactory.filter = _filter;
 
 		SynthFactory.start = function(){
 			return _oscillator.start();
@@ -31,9 +35,17 @@ angular.module('synthApp')
 		SynthFactory.changeFrequency = function(value){
 			_oscillator.frequency.value = value;
 		};
-		
+
 		SynthFactory.changeType = function(type){
 			_oscillator.type = type;
+		};
+
+		SynthFactory.changeLfoType = function(type){
+			lfo.type = type;
+		};
+
+		SynthFactory.filterFrequency = function(frequency){
+			_filter.frequency.value = frequency;
 		};
 
 		SynthFactory.startOscillator = function(){
@@ -43,6 +55,29 @@ angular.module('synthApp')
 		SynthFactory.changeVolume = function(value){
 			_gainNode.gain.value = value;
 		};
+
+		SynthFactory.changeRate = function(rate){
+			console.log(rate);
+			lfo.frequency.value = rate;
+			console.log(_filter.frequency.value);
+		};
+
+		var lfo = context.createOscillator();
+		lfo.frequency.value = 0;
+		lfo.type = 'sine';
+
+
+		var modulationGain = context.createGain();
+		modulationGain.gain.value = 0.15;
+
+		_oscillator.connect(_filter);
+		lfo.connect(modulationGain);
+		modulationGain.connect(_gainNode.gain);
+		lfo.start(0);
+		_filter.connect(_gainNode);
+		_masterFilter.connect(_gainNode);
+		_gainNode.connect(context.destination);
+
 
 		return SynthFactory;
 	});
