ea5defc2ef7272aa65cab69992751370a6c3c8f5
Michael Crowley <miketcrowley@gmail.com>
2017-01-09 17:30:43 -0500

    before first push

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,15 +8,18 @@
  * Controller of the synthApp
  */
 angular.module('synthApp')
-  .controller('MainCtrl', function ($scope, Synth) {
-
+  .controller('MainCtrl', function ($scope, Synth, $window) {
+  $scope.Math = $window.Math;
 	$scope.type = 'sine';
 	$scope.types = ['sine', 'square', 'sawtooth', 'triangle'];
   $scope.changeType = Synth.changeType;
   $scope.changeLfoType = Synth.changeLfoType;
 
   $scope.frequency = 0;
-	$scope.changeFrequency = Synth.changeFrequency;
+  $scope.changeFrequency = Synth.changeFrequency;
+  $scope.filterType = 'lowpass';
+  $scope.filterTypes = ['lowpass', 'highpass', 'bandpass', 'notch'];
+	$scope.changeFilterType = Synth.changeFilterType;
 	$scope.changeFrequency(0);
 	$scope.step = function(frequency){return (frequency/1000);};
 	
@@ -25,10 +28,16 @@ angular.module('synthApp')
 	$scope.changeVolume(0);
   Synth.oscillator.start();
 
+  $scope.changeLfo = Synth.changeLfo;
+  $scope.dests = ['filter', 'volume'];
+
   $scope.filterFrequency = Synth.filterFrequency;
 
   $scope.lfoRate = 0;
+  $scope.lfoType = 'sine';
   $scope.changeRate = Synth.changeRate;
   // $scope.changeRate(0);
+
+  $scope.effectTypes = ['distortion', 'delay'];
 
   });
--- app/scripts/factories/synth-factory.js
+++ app/scripts/factories/synth-factory.js
@@ -9,16 +9,39 @@ angular.module('synthApp')
 		var _oscillator = context.createOscillator();
 		var _gainNode = context.createGain();
 		var _filter = context.createBiquadFilter();
-		_filter.type = _filter.LOWPASS;
+		_filter.type = 'lowpass';
 		var _masterFilter = context.createBiquadFilter();
 		_filter.frequency.value = 300;
 		_masterFilter.frequency.value = 5000;
+		var _masterGain = context.createGain();
 
 		
 
 		_gainNode.gain.value = 0.01;
 		_oscillator.frequency.value = 60;
 		_oscillator.type = 'sine';
+		
+		var lfo = context.createOscillator();
+		lfo.frequency.value = 0;
+		lfo.type = 'sine';
+
+
+		var modulationGain = context.createGain();
+		modulationGain.gain.value = 0.01;
+
+		var modulationFilter = context.createGain();
+		modulationFilter.gain.value = 1000;
+
+		lfo.connect(modulationGain);
+		lfo.connect(modulationFilter);
+		
+		_oscillator.connect(_filter);
+		_filter.connect(_gainNode);
+		
+		_gainNode.connect(_masterGain);
+		_masterGain.connect(context.destination);
+
+		lfo.start(0);
 
 		SynthFactory.oscillator = _oscillator;
 		SynthFactory.gain = _gainNode;
@@ -26,6 +49,20 @@ angular.module('synthApp')
 
 		SynthFactory.start = function(){
 			return _oscillator.start();
+		};
+
+		SynthFactory.changeLfo = function(dest){
+			if(dest === 'filter'){
+				lfo.disconnect();
+				modulationGain.disconnect();
+				lfo.connect(modulationFilter);
+				modulationFilter.connect(_filter.frequency);
+			} else {
+				lfo.disconnect();
+				modulationFilter.disconnect();
+				lfo.connect(modulationGain);
+				modulationGain.connect(_gainNode.gain);
+			}
 		};
 
 		SynthFactory.stop = function(){
@@ -38,6 +75,10 @@ angular.module('synthApp')
 
 		SynthFactory.changeType = function(type){
 			_oscillator.type = type;
+		};
+
+		SynthFactory.changeFilterType = function(type){
+			_filter.type = type;
 		};
 
 		SynthFactory.changeLfoType = function(type){
@@ -53,30 +94,14 @@ angular.module('synthApp')
 		};
 
 		SynthFactory.changeVolume = function(value){
-			_gainNode.gain.value = value;
+		//	_gainNode.gain.value = value;
+			_masterGain.gain.value = value;
 		};
 
 		SynthFactory.changeRate = function(rate){
 			console.log(rate);
 			lfo.frequency.value = rate;
-			console.log(_filter.frequency.value);
 		};
-
-		var lfo = context.createOscillator();
-		lfo.frequency.value = 0;
-		lfo.type = 'sine';
-
-
-		var modulationGain = context.createGain();
-		modulationGain.gain.value = 0.15;
-
-		_oscillator.connect(_filter);
-		lfo.connect(modulationGain);
-		modulationGain.connect(_gainNode.gain);
-		lfo.start(0);
-		_filter.connect(_gainNode);
-		_masterFilter.connect(_gainNode);
-		_gainNode.connect(context.destination);
 
 
 		return SynthFactory;
